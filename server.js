6721b0b8b16a518f48a3452ef0604458ccf5a9f1
Aidan Feldman <aidan.feldman@gmail.com>
2014-09-06 04:32:08 -0400

    send gyro events to server

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('gyro', function(msg){
+    console.log(msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
