a425d60c79b049a7492db5fa5e8c2d8d808de20c
Aidan Feldman <aidan.feldman@gmail.com>
2014-09-08 03:05:40 -0400

    client code organization

--- js/client.js
+++ js/client.js
@@ -1,5 +1,6 @@
 var socket = io();
 
+
 $(window).on('deviceorientation', function(jqEvent) {
   var event = jqEvent.originalEvent;
   var obj = {
@@ -13,39 +14,42 @@ $(window).on('deviceorientation', function(jqEvent) {
 });
 
 
-$(function(){
-  var lastPosByTouchId;
+var lastPosByTouchId;
 
-  var resetLast = function() {
-    lastPosByTouchId = {}
-  };
+var resetLast = function() {
+  lastPosByTouchId = {}
+};
 
-  var $doc = $(document);
-  $doc.on('touchend', resetLast);
-  resetLast();
+var $doc = $(document);
+$doc.on('touchend', resetLast);
+resetLast();
 
-  var handleDrag = function(touch, type){
-    var lastPos = lastPosByTouchId[touch.identifier];
-    if (lastPos) {
-      var dx = touch.screenX - lastPos.x;
-      var dy = touch.screenY - lastPos.y;
+var handleDrag = function(touch, type){
+  var lastPos = lastPosByTouchId[touch.identifier];
+  if (lastPos) {
+    var dx = touch.screenX - lastPos.x;
+    var dy = touch.screenY - lastPos.y;
 
-      // check if it actually moved
-      if (dx || dy) {
-        var drag = {
-          dx: dx,
-          dy: dy,
-          type: type
-        };
-        socket.emit('drag', drag);
-      }
+    // check if it actually moved
+    if (dx || dy) {
+      var drag = {
+        dx: dx,
+        dy: dy,
+        type: type
+      };
+      socket.emit('drag', drag);
     }
-  };
+  }
+};
 
+
+$(function(){
+  // assume zoom area is full height on the left hand side of the screen
   var zoomWidth = $('.zoom').width();
 
   $doc.on('touchmove', function(event){
     $.each(event.originalEvent.changedTouches, function(i, touch) {
+      // check which area this event happened in
       if (touch.clientX > zoomWidth) {
         handleDrag(touch, 'pan');
       } else {
