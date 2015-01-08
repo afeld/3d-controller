636bc94e852fe32e4e8023c594a46bd9e5065c09
Aidan Feldman <aidan.feldman@gsa.gov>
2015-01-07 20:10:41 -0500

    refactor sockets module

--- server/sockets.js
+++ server/sockets.js
@@ -1,22 +1,23 @@
+var broadcastAll = function(socket, channel) {
+  socket.on(channel, function(msg) {
+    msg.sessionId = socket.id;
+    socket.broadcast.emit(channel, msg);
+  });
+};
+
+var onConnection = function(socket) {
+  console.log('client connected');
+
+  broadcastAll(socket, 'gyro');
+  broadcastAll(socket, 'drag');
+
+  socket.on('disconnect', function() {
+    console.log('client disconnected');
+  });
+};
+
+
 module.exports = function(server) {
   var io = require('socket.io')(server);
-
-  var broadcastAll = function(socket, channel) {
-    socket.on(channel, function(msg) {
-      msg.sessionId = socket.id;
-      socket.broadcast.emit(channel, msg);
-    });
-  };
-
-
-  io.on('connection', function(socket) {
-    console.log('client connected');
-
-    broadcastAll(socket, 'gyro');
-    broadcastAll(socket, 'drag');
-
-    socket.on('disconnect', function() {
-      console.log('client disconnected');
-    });
-  });
+  io.on('connection', onConnection);
 };
