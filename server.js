1e969a526526b4c051ae889df81bce38b659b91d
Aidan Feldman <aidan.feldman@gsa.gov>
2015-01-07 20:07:10 -0500

    create socket module

--- server.js
+++ server.js
@@ -1,29 +1,8 @@
 var app = require('./server/app');
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
 
 require('./server/routes');
-
-
-var broadcastAll = function(socket, channel) {
-  socket.on(channel, function(msg) {
-    msg.sessionId = socket.id;
-    socket.broadcast.emit(channel, msg);
-  });
-};
-
-
-io.on('connection', function(socket) {
-  console.log('client connected');
-
-  broadcastAll(socket, 'gyro');
-  broadcastAll(socket, 'drag');
-
-  socket.on('disconnect', function() {
-    console.log('client disconnected');
-  });
-});
-
+require('./server/sockets')(server);
 
 var PORT = 3000;
 server.listen(PORT, function() {
